64b22b9daba3957d0651a817a181999e638c618e
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2023-05-16 14:11:23 +0600

    update description

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     author='Md. Musfiqur Rahaman',
     author_email='musfiqur.rahaman@northsouth.edu',
-    description='This will help you to manage your files and folders easily in different ways.',
+    description='A Python Package to manage your files and folders easily in different ways.',
     packages=['file_managers'],
     install_requires=[]
 )
