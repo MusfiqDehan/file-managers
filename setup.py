df9fb8b04a2bd36d04a8e013f6334bd83f17b0f3
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2023-05-16 16:21:54 +0600

    update version to v0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='file-managers',
-    version='0.1',
+    version='0.2',
     author='Md. Musfiqur Rahaman',
     author_email='musfiqur.rahaman@northsouth.edu',
     description='A Python Package to manage your files and folders easily in different ways.',
