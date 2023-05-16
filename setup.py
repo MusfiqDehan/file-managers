ec34d8b407e942c6a7d1bd55ebfccd086afb47ad
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2023-05-16 14:08:14 +0600

    remove dependencies

--- setup.py
+++ setup.py
@@ -7,8 +7,5 @@ setup(
     author_email='musfiqur.rahaman@northsouth.edu',
     description='This will help you to manage your files and folders easily in different ways.',
     packages=['file_managers'],
-    install_requires=[
-        'numpy',
-        'pandas'
-    ]
+    install_requires=[]
 )
