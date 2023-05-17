421cd1258c51c95d48bbbf038d27f271bf826800
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2023-05-17 13:46:28 +0600

    update setup.py file

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(
     name='file-managers',
     version='0.7.0',
     author='Md. Musfiqur Rahaman',
     author_email='musfiqur.rahaman@northsouth.edu',
     description='A Python Package to manage your files and folders easily in different ways.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=['file_managers'],
     install_requires=['twine', 'wheel'],
     url='https://github.com/MusfiqDehan/file-managers'
-
 )
