b10d8adae2eebb91fe16aa975611815b991f0c5b
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2023-05-19 23:41:55 +0600

    🗑️ delete: changelog.sh

--- changelog.sh
+++
@@ -1,3 +0,0 @@
-export PREVIOUS_VERSION=$(git tag -l --sort=-creatordate | head -n 1)
-export CHANGES=$(git log --pretty="- %s" $PREVIOUS_VERSION..)
-printf "# v${{ steps.current-version.outputs.version }}\n\n## Changes\n$CHANGES\n" > CHANGELOG.md
