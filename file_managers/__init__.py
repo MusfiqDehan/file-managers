99af554a76cd6ed836a9fe49ede0fa54c1119480
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2023-05-24 00:49:27 +0600

    Update CHANGELOG for v0.25.0

--- file_managers/__init__.py
+++ file_managers/__init__.py
@@ -2,9 +2,9 @@ def hello_world1():
     print("Hello, Python!!")
 
 
-def hello_world():
+def hello_world2():
     print("Hello, Python!")
 
 
-def hello_world33():
+def hello_world3():
     print("Hello, Python!!!")
